5d29790da75f0a0d7ea6edde62459a9760bca63a
GavrilS <gavr1lll@abv.bg>
2024-05-04 07:31:17 +0300

    Finished the functionality to close a popup on click outside of it

--- popups/test/app.js
+++ popups/test/app.js
@@ -2,6 +2,7 @@ let activePopup = false;
 const mainComponent = document.querySelector('.container');
 
 const getReportPopup = document.querySelector('#get-report');
+const getReportBtn = document.querySelector('#btn1');
 function getReport() {
     mainComponent.style.opacity = 0.1;
     getReportPopup.classList.add('open-popup');
@@ -9,6 +10,7 @@ function getReport() {
 }
 
 const submitReportPopup = document.querySelector('#send-report');
+const submitReportBtn = document.querySelector('#btn2');
 function submitReport() {
     mainComponent.style.opacity = 0.1;
     submitReportPopup.classList.add('open-popup');
@@ -27,9 +29,11 @@ function closePopup() {
 }
 
 document.body.addEventListener('click', (e) => {
-    const popupClicked = e.target.closest('.popup');
-    console.log('Closest popup element: ', popupClicked);
-    console.log('The body was clicked\n', e.target);
-    console.log(`activePopup: `, activePopup);
-
-})
+    // console.log('Target: ', e.target);
+    if (activePopup&& e.target !== getReportBtn && e.target !== submitReportBtn) {
+        const popupClicked = e.target.closest('.popup');
+        if (!popupClicked) {
+            closePopup();
+        }
+    }
+});
