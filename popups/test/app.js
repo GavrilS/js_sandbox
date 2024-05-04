e81a52b17bf7db7bc50a50dd718dded6e45cf3b8
GavrilS <gavr1lll@abv.bg>
2024-05-04 06:48:23 +0300

    Adding functionality to close open modals when the user clicks outside of the modal

--- popups/test/app.js
+++ popups/test/app.js
@@ -1,8 +1,18 @@
+let activePopup = false;
 const mainComponent = document.querySelector('.container');
+
 const getReportPopup = document.querySelector('#get-report');
 function getReport() {
     mainComponent.style.opacity = 0.1;
     getReportPopup.classList.add('open-popup');
+    activePopup = true;
+}
+
+const submitReportPopup = document.querySelector('#send-report');
+function submitReport() {
+    mainComponent.style.opacity = 0.1;
+    submitReportPopup.classList.add('open-popup');
+    activePopup = true;
 }
 
 function closePopup() {
@@ -13,4 +23,13 @@ function closePopup() {
         elem.classList.remove('open-popup');
         mainComponent.style.opacity = 1;
     }
+    activePopup = false;
 }
+
+document.body.addEventListener('click', (e) => {
+    const popupClicked = e.target.closest('.popup');
+    console.log('Closest popup element: ', popupClicked);
+    console.log('The body was clicked\n', e.target);
+    console.log(`activePopup: `, activePopup);
+
+})
