66b0ff840b040566b8ba17c7a7de84b6f671aff9
GavrilS <gavr1lll@abv.bg>
2024-06-21 23:45:17 +0300

    Writing text input verification methods

---
+++ text_parsing/verify_input/index.js
@@ -0,0 +1,14 @@
+const TEST_INPUT = [
+    '0 0/2 0-23 ? * MON-FRI',
+    '0 0/2 0-3 ? * SAT',
+    '0 0/2 17-23 ? * SUN'
+]
+
+const TEST_INPUT2 = [
+    '0 0/2 0-23 ? * MON-FRI'
+]
+
+const TEST_INPUT3 = [
+    '0 0/2 0-23 ? *'
+]
+
