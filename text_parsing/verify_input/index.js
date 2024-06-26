09f57b263fe6164f9c10db110d89c87089723d85
GavrilS <gavr1lll@abv.bg>
2024-06-26 12:45:12 +0300

    Some minor fixes and test cases added

--- text_parsing/verify_input/index.js
+++ text_parsing/verify_input/index.js
@@ -7,15 +7,15 @@ const WEEKDAY_SEGMENT_PLACE = 5;
 function verifyCronExpression(cronExpr) {
     try{
         console.log('Validating cron expressions: ', cronExpr);
-        let checkLength = checkCronLength(cronExpr);
+        checkCronLength(cronExpr);
         cronExpr.forEach((cron) => {
             let segments = cron.split(' ');
             verifyHourValues(segments[HOUR_SEGMENT_PLACE]);
             verifyWeekdayValues(segments[WEEKDAY_SEGMENT_PLACE]);
         });
     } catch (error) {
-        console.log('Error verifying cron: ', error);
-        return false;
+        // console.log('Error verifying cron: ', error);
+        return error;
     }
     return true;
 }
@@ -28,7 +28,9 @@ function checkCronLength(cronArray) {
     cronArray.forEach((item) => {
         let segments = item.split(' ');
         if (segments.length !== CRON_EXPR_MAX_LENGTH) {
-            throw new Error('Wrong length for cron expression! There needs to be values for 6 segments in a cron expression!');
+            let messageBase = 'Wrong length for cron expression - ' + item
+            let messageAddition = ' There needs to be values for 6 segments in a cron expression!'
+            throw new Error(messageBase + messageAddition);
         }
     });
     return true;
@@ -40,33 +42,45 @@ function checkCronLength(cronArray) {
 const HOUR_SPECIAL_VALUES = [',', '-', '*', '/'];
 const HOUR_MIN_MAX_NUMERIC_VALUES = [0, 23];
 
-function verifyHourValues(cron_hour_segment) {
-    if (cron_hour_segment.includes(HOUR_SPECIAL_VALUES[2]) && 
-        cron_hour_segment.length > 1) {
-        throw new Error('Incorrect use of the * symbol in a cron hour segment!');
-    } else if (cron_hour_segment.includes(HOUR_MIN_MAX_NUMERIC_VALUES[3])) {
-        cron_segment_split = cron_hour_segment.split('/');
-        if (cron_segment_split !== 2) {
-            throw new Error('The special character / can be used only once in a cron segment!');
+function verifyHourValues(cronHourSegment) {
+    if (cronHourSegment.includes(HOUR_SPECIAL_VALUES[2]) && 
+        cronHourSegment.length > 1) {
+        throw new Error('Incorrect use of the * symbol in a cron hour segment: ' + cronHourSegment);
+    } else if (cronHourSegment.includes(HOUR_SPECIAL_VALUES[3])) {
+        let cronSegmentSplit = cronHourSegment.split('/');
+        if (cronSegmentSplit.length !== 2) {
+            throw new Error('The special character / can be used only once in a cron segment: ' + cronHourSegment);
         }
-        
-    } else if (cron_hour_segment.includes(HOUR_SPECIAL_VALUES[0])) {
-        cron_segment_split = cron_hour_segment.split(',');
-        cron_segment_split.forEach(value => {
-            if (value.includes(HOUR_SPECIAL_VALUES[1])) {
+        cronSegmentSplit.forEach(value => {
+            if (value.includes(HOUR_SPECIAL_VALUES[0])) {
+                checkMultipleHourValues(value);
+            } else if (value.includes(HOUR_SPECIAL_VALUES[1])) {
                 checkHourRanges(value);
-            } else {
-                res = Number(value);
-                checkNumericValue(res, HOUR_MIN_MAX_NUMERIC_VALUES[0], HOUR_MIN_MAX_NUMERIC_VALUES[1]);
             }
-        })
-    } else if (cron_hour_segment.includes(HOUR_SPECIAL_VALUES[1])) {
-        checkHourRanges(cron_hour_segment);
+        });
+        
+    } else if (cronHourSegment.includes(HOUR_SPECIAL_VALUES[0])) {
+        checkMultipleHourValues(cronHourSegment);
+    } else if (cronHourSegment.includes(HOUR_SPECIAL_VALUES[1])) {
+        checkHourRanges(cronHourSegment);
     } else {
-        checkNumericValue(cron_hour_segment);
+        checkNumericValue(cronHourSegment, HOUR_MIN_MAX_NUMERIC_VALUES[0], HOUR_MIN_MAX_NUMERIC_VALUES[1]);
     }
 
     return true;
+}
+
+
+function checkMultipleHourValues(cronHourSegment) {
+    let cronSegmentSplit = cronHourSegment.split(',');
+    cronSegmentSplit.forEach(value => {
+        if (value.includes(HOUR_SPECIAL_VALUES[1])) {
+            checkHourRanges(value);
+        } else {
+            let res = Number(value);
+            checkNumericValue(res, HOUR_MIN_MAX_NUMERIC_VALUES[0], HOUR_MIN_MAX_NUMERIC_VALUES[1]);
+        }
+    });
 }
 
 
@@ -77,9 +91,6 @@ function checkHourRanges(range) {
     }
     rangeSplit.forEach(value => {
         let res = Number(value);
-        // if (!res || res < HOUR_MIN_MAX_NUMERIC_VALUES[0] || res > HOUR_MIN_MAX_NUMERIC_VALUES[1]) {
-        //     throw new Error('Wrong hour value supplied in the cron expression!');
-        // }
         checkNumericValue(res, HOUR_MIN_MAX_NUMERIC_VALUES[0], HOUR_MIN_MAX_NUMERIC_VALUES[1])
     });
     return true;
@@ -87,7 +98,7 @@ function checkHourRanges(range) {
 
 function checkNumericValue(value, min, max) {
     if (value < min || value > max) {
-        throw new Error('Wrong hour value supplied in the cron expression!');
+        throw new Error('Wrong hour value supplied in the cron expression - ' + value);
     }
     return true;
 }
@@ -98,16 +109,16 @@ const WEEK_SPECIAL_VALUES = [',','-','*','?'];
 const WEEK_VALUES = ['MON','TUE','WED','THU','FRI','SAT','SUN','1','2','3','4','5','6','7'];
 
 function verifyWeekdayValues(cronWeekSegment) {
-    if (cronWeekSegment.includes(WEEK_SPECIAL_VALUES[2]) ||
-        cronWeekSegment.includes(WEEK_SPECIAL_VALUES[3]) &&
+    if ((cronWeekSegment.includes(WEEK_SPECIAL_VALUES[2]) ||
+        cronWeekSegment.includes(WEEK_SPECIAL_VALUES[3])) &&
         cronWeekSegment.length !== 1
     ) {
-        throw new Error('Wrong usage of * or ? in a cron in the Weekday segment!');
+        throw new Error('Wrong usage of * or ? in a cron in the Weekday segment - ' + cronWeekSegment);
     } else if (cronWeekSegment.includes(WEEK_SPECIAL_VALUES[0])) {
         let cronSplit = cronWeekSegment.split(',');
         cronSplit.forEach(item => {
             if (item.includes(WEEK_SPECIAL_VALUES[1])) {
-                cronItemSplit = item.split('-');
+                let cronItemSplit = item.split('-');
                 cronItemSplit.forEach(itemSplit => {
                     checkWeekdayString(itemSplit);
                 });
@@ -128,8 +139,10 @@ function verifyWeekdayValues(cronWeekSegment) {
 
 
 function checkWeekdayString(value) {
-    if (!WEEK_VALUES.includes(value.toUpperCase())) {
-        throw new Error('Wrong value for Weekday in a cron!');
+    console.log('Weekday value: ', value);
+    if (!WEEK_VALUES.includes(value.toUpperCase()) &&
+        value !== WEEK_SPECIAL_VALUES[2] && value !== WEEK_SPECIAL_VALUES[3]) {
+        throw new Error('Wrong value for Weekday in a cron - ' + value);
     }
     return true;
 }
@@ -140,16 +153,16 @@ function checkWeekdayString(value) {
 
 const PARSE_TEST_INPUT = [
     '0 0/2 0-23 ? * MON-FRI',
-    '0 0/2 0-3 ? * SAT',
+    '0 0/2 8 ? * SAT',
     '0 0/2 17-23 ? * SUN'
 ]
 
 const PARSE_TEST_INPUT2 = [
-    '0 0/2 0-23 ? * MON-FRI'
+    '0 0/2 0-12,18-23 ? * MON-FRI'
 ]
 
 const PARSE_TEST_INPUT3 = [
-    '0 0/2 0-23 ? *'
+    '0 0/2 */* ? * *'
 ]
 
 
